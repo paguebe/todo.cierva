80f6891842d29a1ae6e49e8dc5f6ad28bdd00315
paguebe <pablo.guebe@gmail.com>
2024-10-24 11:49:24 +0200

    DB.php
    mejorado

--- DB.php
+++ DB.php
@@ -2,15 +2,20 @@
 
 class DB {
     public $connection;
+    
     public function __construct() {
         $db_host = "127.0.0.1";
         $db_name = "example_database";
-        $db_user = "example_user2";
-        $db_pass = "password";
-        $this->connection = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);;
+        $db_user = "example_user";
+        $db_pass = "Aprilia";
+        
+        $this->connection = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
     public function __destruct() {
         $this->connection = null;
     }
 }
+?>
+
