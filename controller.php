b7a9aa89a4f78cd50e8190ac61ef27887ef98b1b
paguebe <pablo.guebe@gmail.com>
2024-10-24 11:50:09 +0200

    controller.php
    actualizado 24-10-2024

--- controller.php
+++ controller.php
@@ -1,32 +1,41 @@
+<?php
+require "todo.php";
+require "DB.php";
 
-	<?php
-			require "todo.php";
-		
-		
-			function return_response($status, $statusMessage, $data) {
-				header("HTTP/1.1 $status $statusMessage");
-				header("Content-Type: application/json; charset=UTF-8");
-				echo json_encode($data);
-			}
-			 
-			$bodyRequest = file_get_contents("php://input");
-			
-			switch ($_SERVER['REQUEST_METHOD']) {
-				case 'POST':
-					$db = new DB();
-					$new_todo = new Todo;
-					$new_todo->jsonConstruct($bodyRequest);
-					$new_todo->insert($db->connection); //A falta de programar el insert
-					$todo_list = Todo::DB_selectAll($db->connection);
-					// $todo_list -> Convertir a json y pasarlo en return_response
-					// return_response(200, "OK", $json_del_array_de_todo);
-					break;
-				default:
-					return_response(405, "Method Not Allowed", null);
-					break;
-			}
-	
-		
-		
-	?>
+function return_response($status, $statusMessage, $data) {
+    header("HTTP/1.1 $status $statusMessage");
+    header("Content-Type: application/json; charset=UTF-8");
+    echo json_encode($data);
+}
 
+$bodyRequest = file_get_contents("php://input");
+
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'POST':
+        $db = new DB();
+        $new_todo = new Todo;
+
+        // Inicializar el objeto a partir del cuerpo del POST en JSON
+        $new_todo->jsonConstruct($bodyRequest);
+
+        // Debug: Verificar el contenido que se va a insertar
+        if (empty($new_todo->getContent())) {
+            return_response(400, "Bad Request: Content is empty", null);
+        }
+
+        // Insertar en la base de datos
+        $new_todo->insert($db->connection);
+
+        // Obtener la última tarea insertada
+        $last_todo = Todo::DB_selectAll($db->connection);
+        $last_todo = end($last_todo); // Obtener el último elemento
+
+        // Convertir la última tarea a JSON y devolverla en la respuesta
+        return_response(200, "OK", $last_todo);
+        break;
+
+    default:
+        return_response(405, "Method Not Allowed", null);
+        break;
+}
+?>
