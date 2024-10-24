15ba801ecd106999fba8986e29f437b1b648f14e
paguebe <pablo.guebe@gmail.com>
2024-10-24 11:51:23 +0200

    todo.php
    actualizado a 24-10-24

--- todo.php
+++ todo.php
@@ -2,46 +2,51 @@
 
 class Todo implements \JsonSerializable {
     private int $item_id;
-	private string $content;
-	
-    //Inicializa todas las variables del objeto con las pasadas por parámetros
+    private string $content;
+
+    // Inicializa todas las variables del objeto con las pasadas por parámetros
     public function parametersConstruct(int $item_id, string $content){
         $this->item_id = $item_id;
         $this->content = $content;
     }
 
-    //Inicializa todas las variables con el json pasado por parametro
-	public function jsonConstruct($json) {
-        foreach (json_decode($json, true) AS $key => $value) {
+    // Inicializa todas las variables con el json pasado por parámetro
+    public function jsonConstruct($json) {
+        foreach (json_decode($json, true) as $key => $value) {
             $this->{$key} = $value;
         }
     }
 
-    //Convierte el objeto a un json (jsonEncode)
-	public function jsonSerialize()
-    {
-        $vars = get_object_vars($this);
-        return $vars;
+    // Convierte el objeto a un json (jsonEncode)
+    public function jsonSerialize() {
+        return get_object_vars($this);
     }
-	
-	public function getContent(){		
-		return $this->content;	
-	}
-	
-	public function getItem_id(){		
-		return $this->item_id;
-	}
 
-    //Devuelve todos los elementos de la BBDD en forma de array
-    public static function DB_selectAll($dbconn){
+    public function getContent() {
+        return $this->content;
+    }
+
+    public function getItem_id() {
+        return $this->item_id;
+    }
+
+    // Insertar una nueva tarea en la base de datos
+    public function insert($dbconn) {
+        $stmt = $dbconn->prepare("INSERT INTO todo_list (content) VALUES (:content)");
+        $stmt->bindParam(':content', $this->content);
+        $stmt->execute();
+    }
+
+    // Devuelve todos los elementos de la BBDD en forma de array
+    public static function DB_selectAll($dbconn) {
         $todo_list = array();
-        foreach($dbconn->query("SELECT item_id, content FROM todo_list") as $row) {
+        $stmt = $dbconn->query("SELECT item_id, content FROM todo_list");
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $new_todo = new Todo;
-            $new_todo->parametersConstruct($row['item_id'],$row['content']);
-            $todo_list[]=$new_todo;
+            $new_todo->parametersConstruct($row['item_id'], $row['content']);
+            $todo_list[] = $new_todo;
         }
         return $todo_list;
-
-
     }
 }
+?>
